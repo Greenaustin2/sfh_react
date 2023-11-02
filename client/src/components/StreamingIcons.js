e1906ccc67f3e65959d6e218153f61b594927d6e
Greenaustin2 <greenaustin2@gmail.com>
2023-11-02 16:39:06 -0400

    streaming links no logo

--- client/src/components/StreamingIcons.js
+++ client/src/components/StreamingIcons.js
@@ -4,15 +4,28 @@ import s from "../css/content.module.css";
 const StreamingIcons = (urls) => {
   const url = urls["urls"];
   return (
+    // <div className="content-bottom">
+    //   <a href={url["spotify"]} target="_blank" rel="noreferrer">
+    //     <img alt="spotify icon" id={s.streamingIcon} src={spotify} />
+    //   </a>
+    //   <a href={url["appleMusic"]} target="_blank" rel="noreferrer">
+    //     <img alt="apple music icon" id={s.streamingIcon} src={appleMusic} />
+    //   </a>
+    //   <a href={url["youtubeMusic"]} target="_blank" rel="noreferrer">
+    //     <img alt="youtube music icon" id={s.streamingIcon} src={youtubeMusic} />
+    //   </a>
+    // </div>
     <div className="content-bottom">
       <a href={url["spotify"]} target="_blank" rel="noreferrer">
-        <img alt="spotify icon" id={s.streamingIcon} src={spotify} />
+        spotify
       </a>
+      <p id={s.linkSeparators}> - </p>
       <a href={url["appleMusic"]} target="_blank" rel="noreferrer">
-        <img alt="apple music icon" id={s.streamingIcon} src={appleMusic} />
+        apple
       </a>
+      <p id={s.linkSeparators}> - </p>
       <a href={url["youtubeMusic"]} target="_blank" rel="noreferrer">
-        <img alt="youtube music icon" id={s.streamingIcon} src={youtubeMusic} />
+        youtube
       </a>
     </div>
   );
